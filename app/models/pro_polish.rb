d2c651412745f517b7f537b8d3f50fd269b15d21
McEileen <eileenm@gmail.com>
2016-05-12 22:34:26 -0500

    move constants to pro_polish model

--- app/models/pro_polish.rb
+++ app/models/pro_polish.rb
@@ -1,4 +1,9 @@
-class ProPolish < ActiveRecord::Base
+class ProPolish
+
+RUBY_TERMS = ["object-oriented", "array", "class", "nil", "string", "rspec", "enumerator", "enumerable", "IO", "Hash", "all tests pass", "generator", "Float", "module", "mix-in", "composition", "inheritance", "struct", "Proc", "Lambda", "Method", "Magic Number", "Red, Green, Refactor", "key", "value", "if-else", "conditional statement", "switch statement", "while loop", "infinite loop", "until loop", "do loop", "for loop", "accessor method", "writer method", "reader method", "block", "constant", "global", "local", "instance", "each with index", "each with object", "gsub", "string", "locking constant", "index", "inject", "thread", "TracePoint", "Strong Params", "modulo", "parameters", "override"]
+
+COLORS = ["aubergine", "apple", "auburn", "blush pink", "banana yellow", "badass black", "canteloupe", "coral", "chai", "dandelion", "denim", "dungaree blue", "emerald", "eggplant", "ecru", "fuschia", "forest green", "flamingo pink", "grape", "gunmetal", "green", "heliotrope", "harvest gold", "honeydew", "indigo", "ivory", "iris", "juniper", "jasmine", "jade", "khaki", "kiwi", "key lime", "lavendar", "lemondrop", "lava", "magenta", "mint", "mauve", "navy", "nectarine", "nickel", "obsidian", "ochre", "orchid", "periwinkle", "peach", "pumpkin", "quartz", "quince", "quicksilver", "raspberry", "rose", "raisin", "silver", "silk", "sunset", "titanium", "turquoise", "tangerine", "umber", "ultramarine", "unmellow yellow", "vanilla", "violet", "vermilion", "wisteria", "watermelon", "wintergreen", "xanadu", "xanthic", "xenon", "yellow", "yorkie", "yellow-green", "zuchinni", "zaffre", "zounds"]
+
 
   def initialize
     @ruby_term = RUBY_TERMS.sample
@@ -8,6 +13,5 @@ class ProPolish < ActiveRecord::Base
   def print_color
     "#{@ruby_term} " + "#{@color}"
   end
-
 
 end
